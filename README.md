f548c7fb7444b46eef746d809b85399f6405a09b
Disher Buttar <buttar.dilsher@gmail.com>
2018-08-09 13:37:15 -0700

    added readme file

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # updates
 
 Timeline of updates for a project
+
+GET all updates
+get('/projects/:id/updates')
+
+GET update item
+get('/projects/:id/updates/:updateid')
+
+POST update
+post('/projects/:id/updates/add')
+
+DELETE update
+delete('/projects/:id/updates/:updateid')
+
+PUT update update
+put('/projects/:id/updates/:updateid')
+
+
+
+
--- dataGenerator.js
+++ dataGenerator.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 const faker = require('faker');
 
-const stream = fs.createWriteStream('/Users/Dilsher/Desktop/SDC/updates/projects10.txt');
+const stream = fs.createWriteStream('projects.csv', { flag: 'a'});
 
 stream.once('open', () => {
     const projects = []
     for(let i = 9000001; i < 10000000; i++){
-        projects.push({
-            id : i,
-            date_created: faker.date.past(),
+        stream.write(
+            `${i},
+            ${faker.date.past()},
             ending_date: faker.date.future(),
             backers:faker.random.number(),
             money_raised: faker.finance.amount(),
-            goal: faker.finance.amount(2000,60000,2,'$'),
-        });
+            goal: faker.finance.amount(2000,60000,2,'$')'
+        );
     }
 
     stream.write(JSON.stringify(projects));
